e44cc618abf0556d659562c01d343a3fc238cc50
kunalconfluxsys <kunal.kale@confluxsys.com>
2024-11-25 16:52:37 +0530

    Commit After Changes using Java Implementation

--- src/main/java/com/example/com/graphql/Application.java
+++ src/main/java/com/example/com/graphql/Application.java
@@ -2,9 +2,6 @@ package com.example.com.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.graphql.client.HttpGraphQlClient;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 
 @SpringBootApplication
 public class Application {
@@ -13,8 +10,5 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public HttpGraphQlClient httpGraphQlClient() {
-		return HttpGraphQlClient.builder().url("http://localhost:8080/graphql").build();
-	}
+
 }
